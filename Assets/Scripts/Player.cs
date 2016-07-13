0de90237e98319b0f24ee8e15dcd636c0d3fa85c
Osama Zakarneh <Osama Zakarneh>
2016-07-13 19:36:16 +0300

    Lecture 268: CrossPlatFormInputManager. Done

---
+++ Assets/Scripts/Player.cs
@@ -0,0 +1,20 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityStandardAssets.CrossPlatformInput;
+
+public class Player : MonoBehaviour {
+
+	// Use this for initialization
+	void Start ()
+    {
+	    
+        
+	}
+	
+	// Update is called once per frame
+	void Update ()
+    {
+        print("H: "+ CrossPlatformInputManager.GetAxis("Horizontal"));
+        Debug.Log("V: " + CrossPlatformInputManager.GetAxis("Vertical"));
+	}
+}
